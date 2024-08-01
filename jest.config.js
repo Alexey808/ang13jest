32e417f17d2ba4594c9ab6f2851cedf8f12d10ec
alexey.sv <alexey808sv@gmail.com>
2024-08-02 00:30:37 +0400

    add jest

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/src/setup-jest.ts'],
+  // globals: {
+  //   'ts-jest': {
+  //     allowSyntheticDefaultImports: true,
+  //   },
+  // }
+};
